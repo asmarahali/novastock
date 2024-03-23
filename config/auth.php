ceaab1e1abc6087d4b6d98352b7f5b42ee19a284
asmarahali <a.rahali@esi-sba.dz>
2024-03-23 13:11:33 +0100

    first sprint

---
+++ config/auth.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'defaults' => [
+        'guard' => 'web',
+        'passwords' => 'users',
+    ],
+
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+    ],
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\User::class,
+        ],
+
+        // 'users' => [
+        //     'driver' => 'database',
+        //     'table' => 'users',
+        // ],
+    ],
+    'password_timeout' => 10800,
+
+];
